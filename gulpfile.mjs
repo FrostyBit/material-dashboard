80c0aa1aa9d918af2a662c753365358f85f5602c
tanzy32 <zhiyangtan2107@gmail.com>
2024-12-04 16:47:26 +0800

    updates

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import open from 'gulp-open';
 
 // Task to open the browser
-gulp.task('open-app', function(){
-  gulp.src('pages/dashboard.html')
-  .pipe(open());
+gulp.task('open-app', function () {
+  gulp.src('pages/homepage1.html')
+    .pipe(open());
 });
